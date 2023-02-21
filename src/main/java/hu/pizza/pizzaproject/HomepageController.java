24cb2c52869f2531e0ab16f1673f92e4752192c3
Zsombor Makker <makker.zsombor@gmail.com>
2023-02-21 16:51:46 +0100

    Modifying form now gets User and looks almost right.

--- src/main/java/hu/pizza/pizzaproject/HomepageController.java
+++ src/main/java/hu/pizza/pizzaproject/HomepageController.java
@@ -13,11 +13,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.fxml.FXML;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -42,12 +44,15 @@ public class HomepageController {
     @FXML
     private VBox ablak;
 
-    private String URL = "http://localhost:8080/user";
+    @FXML
+    private TableView<User> lista = new TableView<>();
+
+    private int Update_id;
+
+    private String USER_URL = "http://localhost:8080/user";
 
     @FXML
     private void initialize() {
-        //TODO: Emberünk nevének kiírása
-
         // JWT token kiolvasás
         String jsonToken = ApplicationConfiguration.getJwtToken().getJwtToken();
 
@@ -66,7 +71,7 @@ public class HomepageController {
         try {
             // Prepare the request
             dataRequest = HttpRequest.newBuilder()
-                    .uri(new URI(URL + "/data"))
+                    .uri(new URI(USER_URL + "/data"))
                     .header("Content-Type", "application/json")
                     .header("Authorization", "Bearer " + jsonToken)
                     .GET()
@@ -121,7 +126,120 @@ public class HomepageController {
     }
 
     public void modositasClick(ActionEvent actionEvent) {
-        //TODO: ha kivan jelölve 1 pizza/ember adatBox-ba form létrehozás modosításhoz
+        // Kijelölés ellenőrzése
+        int selectedIndex = lista.getSelectionModel().getSelectedIndex();
+        Window owner = kilepesButton.getScene().getWindow();
+        if (selectedIndex == -1){
+            showAlert(Alert.AlertType.ERROR, owner, "Használati hiba!","Először jelöljön ki egy elemet!");
+        }else{
+            //TODO: modosítás ablak elkészítése
+
+            // adatok mentése
+            User selected = lista.getSelectionModel().getSelectedItem();
+            User modifyingUser = new User(selected.getId(), selected.getFirst_name(), selected.getLast_name(), selected.getEmail(), selected.getPassword(), selected.isAdmin());
+
+            // Sceneben form létrehozása
+            VBox kisablakVbox = new VBox(10);
+            HBox firstNameSor = new HBox(10);
+            HBox lastNameSor = new HBox(10);
+            HBox emailSor = new HBox(10);
+            HBox passwordSor = new HBox(10);
+            HBox adminSor = new HBox(10);
+
+            // Labelek
+            Label firstName = new Label();
+            firstName.setText("Firstname:");
+            Label lastName = new Label();
+            lastName.setText("Lastname:");
+            Label email = new Label();
+            email.setText("Email:");
+            Label password = new Label();
+            password.setText("Password:");
+            Label admin = new Label();
+            admin.setText("Admin:");
+
+            // Inputok
+            TextField firstNameTextField = new TextField();
+            TextField lastNameTextField = new TextField();
+            TextField emailTextField = new TextField();
+            TextField passwordTextField = new TextField();
+            CheckBox adminCheckbox = new CheckBox();
+            Button keszButton = new Button();
+            // Inputokba value-k:
+            firstNameTextField.setText(modifyingUser.getFirst_name());
+            lastNameTextField.setText(modifyingUser.getLast_name());
+            emailTextField.setText(modifyingUser.getEmail());
+            passwordTextField.setText(modifyingUser.getPassword());
+            keszButton.setText("Mentés");
+            if (modifyingUser.isAdmin()){
+                adminCheckbox.setSelected(true);
+            }else{
+                adminCheckbox.setSelected(false);
+            }
+
+            // sceneClearing
+            adatokBoxClear();
+
+            // kialakítás design:
+            adatokBox.setAlignment(Pos.CENTER);
+            kisablakVbox.setAlignment(Pos.TOP_CENTER);
+
+            kisablakVbox.setPadding(new Insets(0, 320, 0, 0));
+            kisablakVbox.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 2px;");
+
+            // Hboxok
+            firstNameSor.setAlignment(Pos.TOP_RIGHT);
+            lastNameSor.setAlignment(Pos.TOP_RIGHT);
+            emailSor.setAlignment(Pos.TOP_RIGHT);
+            passwordSor.setAlignment(Pos.TOP_RIGHT);
+            adminSor.setAlignment(Pos.TOP_RIGHT);
+            // Kell Egy node a marginhoz
+
+            // labelek
+            firstName.setPadding(new Insets(10, 10, 0, 0));
+            lastName.setPadding(new Insets(10, 10, 0, 0));
+            email.setPadding(new Insets(10, 10, 0, 0));
+            password.setPadding(new Insets(10, 10, 0, 0));
+            admin.setPadding(new Insets(10, 10, 0, 0));
+
+            // Sorok:
+            firstNameSor.setPadding(new Insets(10, 0, 0, 0));
+
+            lastNameSor.setPadding(new Insets(10, 0, 0, 0));
+
+            emailSor.setPadding(new Insets(10, 0, 0, 0));
+
+            passwordSor.setPadding(new Insets(10, 0, 0, 0));
+
+            adminSor.setPadding(new Insets(10,0,0,0));
+
+            keszButton.setPadding(new Insets(0,20,0,20));
+
+
+            // Hboxokba label + inputok
+            firstNameSor.getChildren().addAll(firstName,firstNameTextField);
+            lastNameSor.getChildren().addAll(lastName,lastNameTextField);
+            emailSor.getChildren().addAll(email,emailTextField);
+            passwordSor.getChildren().addAll(password,passwordTextField);
+            adminSor.getChildren().addAll(admin,adminCheckbox);
+
+            // Vboxba a hboxok
+            kisablakVbox.getChildren().addAll(firstNameSor, lastNameSor, emailSor, passwordSor, adminSor, keszButton);
+            adatokBox.getChildren().add(kisablakVbox);
+
+            // Modosítások után backendre felrakás (json, és pontosasn egyezzen a User class mintájával)
+
+        }
+
+    }
+
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
     }
 
     public void torlesClick(ActionEvent actionEvent) {
@@ -131,7 +249,6 @@ public class HomepageController {
     private void adatokBoxClear(){
         adatokBox.getChildren().clear();
     }
-
 
     public void pizzaListing(ActionEvent actionEvent) {
         //TODO: Pizza kilistázás
@@ -152,9 +269,6 @@ public class HomepageController {
         text.setFont(Font.font(15));
         adatokBox.getChildren().add(text);
         adatokBox.setAlignment(Pos.TOP_CENTER);
-
-        // TableView hozzáadás
-        TableView lista = new TableView();
 
         // id
         TableColumn<User ,Integer> column1 =
@@ -198,12 +312,7 @@ public class HomepageController {
         column6.setCellValueFactory(
                 new PropertyValueFactory<>("admin"));
 
-        lista.getColumns().add(column1);
-        lista.getColumns().add(column2);
-        lista.getColumns().add(column3);
-        lista.getColumns().add(column4);
-        lista.getColumns().add(column5);
-        lista.getColumns().add(column6);
+        lista.getColumns().addAll(column1,column2,column3,column4,column5,column6);
 
         adatokBox.getChildren().add(lista);
 
@@ -222,7 +331,7 @@ public class HomepageController {
         try {
             // Prepare the request
             usersrequest = HttpRequest.newBuilder()
-                    .uri(new URI(URL + "/get-all"))
+                    .uri(new URI(USER_URL + "/get-all"))
                     .header("Content-Type", "application/json")
                     .GET()
                     .build();
