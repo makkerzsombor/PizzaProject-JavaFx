eb777314e963211bde1fc3af04d5c3b1f075e9c5
Zsombor Makker <makker.zsombor@gmail.com>
2023-03-09 10:51:45 +0100

    FormsAndLists updated - Can list Pizzas, Users and can add pizzas as well

--- src/main/java/hu/pizza/pizzaproject/HomepageController.java
+++ src/main/java/hu/pizza/pizzaproject/HomepageController.java
@@ -308,7 +308,7 @@ public class HomepageController {
         // Táblázat újra generálása
         userListCreate();
     }
-    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+    public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
         alert.setHeaderText(null);
@@ -392,7 +392,6 @@ public class HomepageController {
         // Táblázat ujra kreálása:
         userListCreate();
     }
-
     private void pizzaVeglegesTorles(long selectedIndex) {
         System.out.println("Pizzás delete-be megy");
         // Törlés
@@ -408,7 +407,6 @@ public class HomepageController {
         // Táblázat ujra kreálása:
         pizzaListCreate();
     }
-
     private void adatokBoxClear() {
         adatokBox.getChildren().clear();
     }
@@ -419,218 +417,35 @@ public class HomepageController {
         addPizzaFormCreate();
     }
     private void addPizzaFormCreate() {
-        // Sceneben form létrehozása (A keszButton kell, mert csak így lehet margint állítani)
-        VBox kisablakVbox = new VBox(10);
-
-        // Név
-        Label nev = new Label();
-        nev.setText("Név:");
-        TextField nevTextField = new TextField();
-        HBox nevSor = new HBox(10, nev, nevTextField);
-
-        // Leírás
-        Label leiras = new Label();
-        leiras.setText("Leírás:");
-        TextField leirasTextField = new TextField();
-        HBox leirasSor = new HBox(10, leiras, leirasTextField);
-
-        // Kép
-        Label kep = new Label();
-        kep.setText("Kép:");
-        TextField kepTextField = new TextField();
-        HBox kepSor = new HBox(10, kep, kepTextField);
-
-        // Ár
-        Label Ár = new Label();
-        Ár.setText("Ár:");
-        Spinner<Integer> arField = new Spinner<>();
-        arField.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100000, 2000, 10));
-        HBox arSor = new HBox(10, Ár, arField);
-
-        // Létrehozás
-        Button keszButton = new Button();
-        keszButton.setText("Létrehozás");
-        HBox buttonSor = new HBox(keszButton);
-        HBox.setMargin(keszButton, new Insets(0, 120, 10, 0));
-        // sceneClearing
         adatokBoxClear();
-
-        // kialakítás design:
-        adatokBox.setAlignment(Pos.CENTER);
-        kisablakVbox.setAlignment(Pos.TOP_CENTER);
-
-        kisablakVbox.setPadding(new Insets(0, 320, 10, 0));
-        kisablakVbox.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 2px;");
-
-        // Hboxok
-        nevSor.setAlignment(Pos.TOP_RIGHT);
-        leirasSor.setAlignment(Pos.TOP_RIGHT);
-        kepSor.setAlignment(Pos.TOP_RIGHT);
-        arSor.setAlignment(Pos.TOP_RIGHT);
-        buttonSor.setAlignment(Pos.TOP_RIGHT);
-
-        // labelek
-        nev.setPadding(new Insets(5, 0, 0, 0));
-        leiras.setPadding(new Insets(5, 0, 0, 0));
-        kep.setPadding(new Insets(5, 0, 0, 0));
-        Ár.setPadding(new Insets(5, 0, 0, 0));
-        keszButton.setPadding(new Insets(0, 20, 0, 20));
-
-        // Sorok:
-        nevSor.setPadding(new Insets(10, 0, 0, 0));
-        leirasSor.setPadding(new Insets(10, 0, 0, 0));
-        kepSor.setPadding(new Insets(10, 0, 0, 0));
-        arSor.setPadding(new Insets(10, 0, 0, 0));
-
-        // Vboxba a hboxok
-        kisablakVbox.getChildren().addAll(nevSor, leirasSor, kepSor, arSor, buttonSor);
-        adatokBox.getChildren().add(kisablakVbox);
-
-        keszButton.setOnAction((event) -> {
-            if (nevTextField.getText().equals("") || leirasTextField.getText().equals("") || kepTextField.getText().equals("")) {
-                Window owner = kilepesButton.getScene().getWindow();
-                showAlert(Alert.AlertType.ERROR, owner, "Használati hiba!", "Töltsön ki minden mezőt!");
-            } else {
-                Pizza newPizza = new Pizza(nevTextField.getText(), kepTextField.getText(),leirasTextField.getText(), arField.getValue());
-                HttpResponse response = requestHandler.addPizzaRequest(PIZZA_URL, newPizza);
-                if (response.statusCode() == 200) {
-                    System.out.println("Pizza sikeresen létrehozva");
-                    Window window = adatokBox.getScene().getWindow();
-                    showAlert(Alert.AlertType.CONFIRMATION, window, "Sikeres létrehozás", "Az adott pizzát sikeresen létrehoztuk");
-                } else {
-                    System.out.println("Nem jó");
-                    Window window = adatokBox.getScene().getWindow();
-                    showAlert(Alert.AlertType.CONFIRMATION, window, "Hiba történt", "Dolgozunk rajta!");
-                }
-                pizzaListCreate();
-            }
-        });
-
+        //Létrehozott formandlist
+        FormsAndLists formsAndLists = new FormsAndLists(adatokBox);
+        adatokBox.getChildren().add(formsAndLists.createPizza(kilepesButton));
     }
     private void userListCreate() {
         userTable = true;
         adatokBoxClear();
         userLista.getItems().clear();
         userLista.getColumns().clear();
-
-        // Tábla cím
-        Text text = new Text();
-        text.setText("User adatok");
-        adatokBox.setMargin(text, new Insets(0, 0, 10, 0));
-        text.setStyle("-fx-fill: white; ");
-        text.setFont(Font.font("Segoe UI", FontWeight.BOLD, 15));
-        adatokBox.getChildren().add(text);
-        adatokBox.setAlignment(Pos.TOP_CENTER);
-
-        // id
-        TableColumn<User, Integer> column1 =
-                new TableColumn<>("Id");
-
-        column1.setCellValueFactory(
-                new PropertyValueFactory<>("Id"));
-
-        // email
-        TableColumn<User, String> column2 =
-                new TableColumn<>("Email");
-
-        column2.setCellValueFactory(
-                new PropertyValueFactory<>("Email"));
-
-        // lastname
-        TableColumn<User, String> column3 =
-                new TableColumn<>("Lastname");
-
-        column3.setCellValueFactory(
-                new PropertyValueFactory<>("last_name"));
-
-        // firstname
-        TableColumn<User, String> column4 =
-                new TableColumn<>("Firstname");
-
-        column4.setCellValueFactory(
-                new PropertyValueFactory<>("first_name"));
-
-        // admin
-        TableColumn<User, Boolean> column5 =
-                new TableColumn<>("Admin");
-
-        column5.setCellValueFactory(
-                new PropertyValueFactory<>("admin"));
-
-        userLista.getColumns().addAll(column1, column2, column3, column4, column5);
-
-        adatokBox.getChildren().add(userLista);
-
-        List<User> userLista = requestHandler.getallUserRequest(USER_URL);
-        // Listából tableViewba rakás
-        this.userLista.setItems(FXCollections.observableArrayList(userLista));
+        //Létrehozott formandlist
+        FormsAndLists formsAndLists = new FormsAndLists(adatokBox);
+        adatokBox.getChildren().add(formsAndLists.createUserList(userLista));
     }
     private void pizzaListCreate() {
         userTable = false;
         adatokBoxClear();
         pizzaLista.getItems().clear();
         pizzaLista.getColumns().clear();
-
-        // Tábla cím
-        Text text = new Text();
-        text.setText("Pizza adatok");
-        adatokBox.setMargin(text, new Insets(0, 0, 10, 0));
-        text.setStyle("-fx-fill: white; ");
-        text.setFont(Font.font("Segoe UI", FontWeight.BOLD, 15));
-        adatokBox.getChildren().add(text);
-        adatokBox.setAlignment(Pos.TOP_CENTER);
-
-        // id
-        TableColumn<Pizza, Integer> column1 =
-                new TableColumn<>("Id");
-
-        column1.setCellValueFactory(
-                new PropertyValueFactory<>("Id"));
-
-        // name
-        TableColumn<Pizza, String> column2 =
-                new TableColumn<>("Name");
-
-        column2.setCellValueFactory(
-                new PropertyValueFactory<>("name"));
-
-        // description
-        TableColumn<Pizza, String> column3 =
-                new TableColumn<>("Description");
-
-        column3.setCellValueFactory(
-                new PropertyValueFactory<>("description"));
-
-        // picture
-        TableColumn<Pizza, String> column4 =
-                new TableColumn<>("Picture");
-
-        column4.setCellValueFactory(
-                new PropertyValueFactory<>("picture"));
-
-        // price
-        TableColumn<Pizza, Integer> column5 =
-                new TableColumn<>("Price");
-
-        column5.setCellValueFactory(
-                new PropertyValueFactory<>("price"));
-
-        pizzaLista.getColumns().addAll(column1, column2, column3, column4, column5);
-
-        adatokBox.getChildren().add(pizzaLista);
-        List<Pizza> pizzaListaKesz = requestHandler.getallPizzaRequest(PIZZA_URL);
-        // Listából tableViewba rakás
-        this.pizzaLista.setItems(FXCollections.observableArrayList(pizzaListaKesz));
+        FormsAndLists formsAndLists = new FormsAndLists(adatokBox);
+        adatokBox.getChildren().add(formsAndLists.createPizzaList(pizzaLista));
     }
     public void userListing(ActionEvent actionEvent) {
         userListCreate();
     }
-
     public void rendelesClick(ActionEvent actionEvent) {
         rendelesFormFrissites();
     }
     public void rendelesFormFrissites(){
-        //TODO: Ezt kellene meghivni valahogy
         FormsAndLists formsAndLists = new FormsAndLists(adatokBox);
         adatokBoxClear();
         adatokBox.setAlignment(Pos.TOP_CENTER);
